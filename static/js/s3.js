ebb56ef27899fe61622abf8f6f8c8502750f6fc7
Alexander Mendes <alexanderhmendes@gmail.com>
2017-02-09 17:49:53 +0000

    Update S3 importer

--- static/js/s3.js
+++ static/js/s3.js
@@ -6,39 +6,41 @@ $('#get-s3-bucket').on('click', function() {
     $('#objects').empty();
     $.get('/amazon/bucket/'+ bucketName).done(function(objects) {
         objects.forEach(function(object) {
-            objectElement = $('<li class="object" role="button" style="cursor:pointer;">' + object + '</li>');
+            objectElement = $('<tr class="object" style="cursor:pointer;"><td>' + object + '</td></tr>');
                     $('#objects').append(objectElement);
             });
             
             $(".object").each(function(){
-                    let objectDomElement = $(this),
-                        objectName       = $(this).text();
-                    objectDomElement.off('click').on('click', function(evt) {
-                        evt.preventDefault();
-                        objectDomElement.toggleClass('selected');
-                        if (objectDomElement.hasClass('selected')) {
-                            if (files.indexOf(objectName) === -1) {
-                                files.push(objectName);
-                            }
+                let objectDomElement = $(this),
+                    objectName       = $(this).text();
+                objectDomElement.off('click').on('click', function(evt) {
+                    evt.preventDefault();
+                    objectDomElement.toggleClass('table-success');
+                    if (objectDomElement.hasClass('table-success')) {
+                        if (files.indexOf(objectName) === -1) {
+                            files.push(objectName);
                         }
-                        else {
-                            files.splice(files.indexOf(objectName), 1);
-                        }
-                        filesInput.innerHTML = null;
-                        files.forEach(function(file, index){
-                            let element = document.createElement("li"),
-                                content = document.createTextNode(file),
-                                input   = document.createElement('input');
-                            input.id = "files-" + index;
-                            input.type = "hidden";
-                            input.name = "files-" + index;
-                            input.value = file;
-                            element.appendChild(input);
-                            element.appendChild(content);
-                            filesInput.appendChild(element);
-                        });
+                    }
+                    else {
+                        files.splice(files.indexOf(objectName), 1);
+                    }
+                    filesInput.innerHTML = null;
+                    files.forEach(function(file, index){
+                        let element = document.createElement("li"),
+                            content = document.createTextNode(file),
+                            input   = document.createElement('input');
+                        input.id = "files-" + index;
+                        input.type = "hidden";
+                        input.name = "files-" + index;
+                        input.value = file;
+                        element.appendChild(input);
+                        element.appendChild(content);
+                        filesInput.appendChild(element);
                     });
                 });
+            });
+            
+            $('#objects-container').show();
             }).fail(function(resp) {
                 if (resp.status === 403) {
                         notify("Oops... it seems that bucket is private.", 'error');

d58e64279dcbd8bf22df3c0cb32d86feeaefd56e
Alexander Mendes <alexanderhmendes@gmail.com>
2017-02-16 10:48:23 +0000

    Update error messages

--- static/js/log-error.js
+++ static/js/log-error.js
@@ -2,7 +2,7 @@
  * Show report dialog for SentryIO.
  */
 function logError(err, showReportDialog=true) {
-    notify('Error: ' + err.message, 'error');
+    notify(err.message, 'error');
     if (typeof Raven !== 'undefined' && Raven.isSetup()) {
         event_id = Raven.captureException(err);
         if (showReportDialog) {

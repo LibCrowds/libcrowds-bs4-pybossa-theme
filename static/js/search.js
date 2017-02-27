4ce502adfcbf488638ce76ab007d6e4c333cc317
Alexander Mendes <alexanderhmendes@gmail.com>
2017-02-27 14:02:30 +0000

    Update search tables

--- static/js/search.js
+++ static/js/search.js
@@ -1,12 +1,23 @@
-$('.search[data-target]').on('keyup', function(evt) {
-    const searchText = evt.target.value.toLowerCase(),
-          target     = $(this).data('target');
+$('.search-table[data-target]').on('keyup', function(evt) {
+    const tableId    = $(this).data('target'),
+          searchText = evt.target.value.toLowerCase(),
+          columns    = $(tableId).find('thead tr th').length;
+    console.log(columns);
 
-    $(target).each(function() {
+    $(tableId).find('tbody tr.no-data').remove();
+
+    $(tableId).find('tbody tr').each(function() {
         if ($(this).text().toLowerCase().indexOf(searchText) !== -1) {
             $(this).show();
         } else {
             $(this).hide();
         }
     });
+
+    if (!$(tableId).find('tbody tr:visible').length) {
+        const noDataRow = $(`<tr class="no-data text-center">
+                            <td colspan="${columns}">No data to show</td>
+                            </tr>`);
+        $(tableId).find('tbody').append(noDataRow);
+    }
 });

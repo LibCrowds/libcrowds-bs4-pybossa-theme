8c7b3c7e521acd394f27a28138d0eaf84c2ead83
Alexander Mendes <alexanderhmendes@gmail.com>
2017-02-27 14:56:10 +0000

    Use CSS to display no data row in search tables

--- static/js/search.js
+++ static/js/search.js
@@ -1,23 +1,12 @@
 $('.search-table[data-target]').on('keyup', function(evt) {
     const tableId    = $(this).data('target'),
-          searchText = evt.target.value.toLowerCase(),
-          columns    = $(tableId).find('thead tr th').length;
-    console.log(columns);
+          searchText = evt.target.value.toLowerCase();
 
-    $(tableId).find('tbody tr.no-data').remove();
-
-    $(tableId).find('tbody tr').each(function() {
+    $(tableId).find('tbody tr:not(.no-data)').each(function() {
         if ($(this).text().toLowerCase().indexOf(searchText) !== -1) {
-            $(this).show();
+            $(this).removeClass('hidden');
         } else {
-            $(this).hide();
+            $(this).addClass('hidden');
         }
     });
-
-    if (!$(tableId).find('tbody tr:visible').length) {
-        const noDataRow = $(`<tr class="no-data text-center">
-                            <td colspan="${columns}">No data to show</td>
-                            </tr>`);
-        $(tableId).find('tbody').append(noDataRow);
-    }
 });

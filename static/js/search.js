1f2cd595e8964f8ec3ccf2e136101bb57eadb5e8
Alexander Mendes <alexanderhmendes@gmail.com>
2017-02-27 15:16:09 +0000

    Search tables by word

--- static/js/search.js
+++ static/js/search.js
@@ -1,12 +1,22 @@
 $('.search-table[data-target]').on('keyup', function(evt) {
     const tableId    = $(this).data('target'),
           searchText = evt.target.value.toLowerCase();
+          searchWords = searchText.split(' ');
 
-    $(tableId).find('tbody tr:not(.no-data)').each(function() {
-        if ($(this).text().toLowerCase().indexOf(searchText) !== -1) {
-            $(this).removeClass('hidden');
-        } else {
-            $(this).addClass('hidden');
-        }
-    });
+        $(tableId).find('tbody tr:not(.no-data)').each(function() {
+            checkRow($(this), searchWords);
+        });
 });
+
+/**
+ * Hide a row if it doesn't contain all search words.
+ */
+function checkRow(row, searchWords) {
+    for (let word of searchWords) {
+        if (row.text().toLowerCase().indexOf(word) === -1) {
+            row.addClass('hidden');
+            return;
+        }
+    }
+    row.removeClass('hidden');
+}

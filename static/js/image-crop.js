a8f3d3097b1f3cee9b0b00a25ce60334289945aa
Alexander Mendes <alexanderhmendes@gmail.com>
2017-03-02 17:01:05 +0000

    Update account pages

---
+++ static/js/image-crop.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+    let $uploadCrop;
+    let $preview = $('#uploadPreview');
+
+    function readFile(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function (evt) {
+                $uploadCrop.croppie('bind', {
+                    url: evt.target.result
+                });
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $uploadCrop = $preview.croppie({
+        viewport: {
+            width: $preview.width() || 300,
+            height: $preview.height() || 300,
+            type: $preview.hasClass('crop-circle') ? 'circle' : 'square'
+        },
+        boundary: {
+            width: $preview.width() || 300,
+            height: $preview.height() || 300
+        },
+        update: function (data) {
+            $("#x1").val(Math.floor(data.points[0]));
+            $("#y1").val(Math.floor(data.points[1]));
+            $("#x2").val(Math.floor(data.points[2]));
+            $("#y2").val(Math.floor(data.points[3]));
+        }
+    });
+
+    $('#avatar').on('change', function () { readFile(this); });
+});

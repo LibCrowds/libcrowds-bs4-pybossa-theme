e27030f7b288fcab08b86ac402d24b4395c3898d
Alexander Mendes <alexanderhmendes@gmail.com>
2017-03-03 12:19:54 +0000

    Update project pages

---
+++ static/js/webhook.js
@@ -0,0 +1,16 @@
+$('.send-webhook').on('click', function() {
+    const projectShortName = $(this).data('project-short-name'),
+          webhookId        = $(this).data('webhook-id'),
+          buttonHtml       = $(this).html(),
+          row              = $(this).parents('tr');
+    $(this).html('<span class="fa fa-spinner fa-spin"></span>');
+    $.ajax({
+        type: 'POST',
+        url: `/project/${projectShortName}/webhook/${webhookId}`,
+        dataType: 'json'
+    }).done((data) => {
+        console.log(data);
+        $(this).html(buttonHtml);
+        notify('Webhook sent!', 'success');
+    });
+});

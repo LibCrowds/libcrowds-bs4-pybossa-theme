0f2b774da7a6ba3f98595087e4497106483c2db5
Alexander Mendes <alexanderhmendes@gmail.com>
2017-03-09 00:32:35 +0000

    More project page updates

---
+++ templates/_includes/user-rank.js
@@ -0,0 +1,20 @@
+<script>
+const getUserRank = function() {
+    const currentUserName = '{{ current_user.name }}' || null;
+    let def = $.Deferred();
+    $.ajax({
+        url: '/leaderboard/',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: '{}'
+    }).done(function(data) {
+        for (let user of data.top_users) {
+            if (user.name == currentUserName) {
+                def.resolve([user.rank, user.score]);
+            }
+        }
+        def.resolve(null);
+    });
+    return def;
+}
+</script>

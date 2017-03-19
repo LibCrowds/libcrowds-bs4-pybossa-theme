34dee759047e5c7a8e1b950d99c11c1eb997d7e1
Alexander Mendes <alexanderhmendes@gmail.com>
2017-03-19 16:15:32 +0000

    Reorganise assets

--- static/js/image-error.js
+++ static/js/image-error.js
@@ -1,7 +1,5 @@
-$(document).ready(function() {
-    $('img').on("error", function () {
-        const rand = Math.floor((Math.random() * 10) + 1),
-              url  = 'http://placekitten.com/400/400?image=' + rand;
-        $(this).attr('src', url);
-    });
+$('img').on("error", function () {
+    const rand = Math.floor((Math.random() * 10) + 1),
+          url  = 'http://placekitten.com/400/400?image=' + rand;
+    $(this).attr('src', url);
 });

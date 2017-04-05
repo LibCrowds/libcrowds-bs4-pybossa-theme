eae071260c89c2a90d3f1b1393143267528d0110
Alex Mendes <alex.mendes@bl.uk>
2017-04-05 18:29:57 +0000

    Improve affordance of project cards

--- static/js/progress.js
+++ static/js/progress.js
@@ -1,11 +1,18 @@
-/**
- * Initialise progress circles.
- */
-$('.progress-circle').each(function() {
+// Initialise progress circles.
+$('.progress-circle:not(.progress-circle-white)').each(function() {
     var value = parseFloat($(this).data('progress') / 100);
     $(this).circleProgress({
         value: value,
         fill: '#D00000',
+        size: 180
+    });
+});
+
+$('.progress-circle-white').each(function() {
+    var value = parseFloat($(this).data('progress') / 100);
+    $(this).circleProgress({
+        value: value,
+        fill: '#FFF',
         size: 180
     });
 });

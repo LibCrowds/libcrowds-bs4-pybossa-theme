8a49531d60f5832b6031eeea09b25cf189b3eb76
Alex Mendes <alex.mendes@bl.uk>
2017-04-05 08:24:54 +0000

    Update homepage

--- static/js/anchor-scroll.js
+++ static/js/anchor-scroll.js
@@ -5,7 +5,7 @@ $('a[href*=\\#]:not([href="#"])').on('click', function(evt) {
     let anchor = $($(this).attr('href'));
     setTimeout(function() {
         $('html, body').animate({
-            scrollTop: anchor.offset().top - 40
+            scrollTop: anchor.offset().top
         }, 1000);
     }, 100);
     evt.preventDefault();

1901b5075771e6248814d99ed392793294d95f94
Alex Mendes <alex.mendes@bl.uk>
2017-04-05 16:38:35 +0000

    Use a different hamburger for larger screens

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,14 +1,41 @@
 $('.hamburger').on('click', function(evt) {
-    var targetId = $(this).data('target'),
-        timeout  = $(this).hasClass('is-active') ? 50 : 300,
-		_this    = $(this);
+	switchIcons($(this));
+	switchColours($(this));
+});
+
+$(window).on("resize", function() {
+    $('.hamburger').each(function() {
+		switchIcons($(this));
+	});
+});
+
+/**
+ * Handle colour changes for full width menus.
+ */
+function switchColours(elem) {
+	var targetId = elem.data('target'),
+        timeout  = elem.hasClass('is-active') ? 50 : 300;
     
-	
 	$(targetId).toggleClass('show');
-    $(this).toggleClass('is-active');
-    $(this).blur();
+    elem.toggleClass('is-active');
+    elem.blur();
     
     setTimeout(function() {
-        _this.toggleClass('white');
+        elem.toggleClass('white');
     }, timeout);
-});
+}
+
+
+/**
+ * Use different icons for full and non-full width menus.
+ */
+ function switchIcons(elem) {
+	 console.log(1);
+	if ($(window).width() > 992) {
+		elem.removeClass('hamburger--collapse');
+		elem.addClass('hamburger--arrow');
+	} else {
+		elem.addClass('hamburger--collapse');
+		elem.removeClass('hamburger--arrow');
+	}
+ }

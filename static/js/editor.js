a7517d9fb7468680f6bb3ca3e3ee55376fe42151
Alex Mendes <alex.mendes@bl.uk>
2017-04-12 13:57:31 +0000

    Update code editor styles and layout

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,10 +1,25 @@
 /**
- * Initialise code editor;
+ * Initialise code editor for text areas.
  */
-CodeMirror.fromTextArea(document.getElementById('editor'), {
-    mode:'text/html',
-    lineNumbers: true,
-    autofocus: true,
-    lineWrapping: true,
-    scrollbarStyle: null,
+if ($('#editor').length) {
+    CodeMirror.fromTextArea(document.getElementById('editor'), {
+        mode:'text/html',
+        lineNumbers: true,
+        autofocus: true,
+        lineWrapping: true,
+        scrollbarStyle: null,
+    });
+ }
+
+/**
+ * Initialise code editor for code blocks.
+ */
+ $('code').each(function() {
+    var code = $(this).html(),
+        opts = $(this).data();
+    
+    opts.value = code;
+    $(this).empty();
+
+    CodeMirror(this, opts);
 });

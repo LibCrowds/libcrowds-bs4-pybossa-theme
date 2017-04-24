607f29399551c1a6c239b37ac3db18bb5c59fd8c
Alexander Mendes <alexanderhmendes@gmail.com>
2017-04-24 23:57:16 +0100

    Continue table integration

--- static/js/search.js
+++ static/js/search.js
@@ -1,22 +1,76 @@
-$('.search-table[data-target]').on('keyup', function(evt) {
-    var tableId = $(this).data('target'),
-        text    = evt.target.value.toLowerCase(),
-        words   = text.split(' ');
-
-        $(tableId).find('tbody tr:not(.no-data)').each(function() {
-            checkRow($(this), words);
-        });
-});
-
 /**
- * Hide a row if it doesn't contain all search words.
+ * A class for handling shuffleable tables.
  */
-function checkRow(row, words) {
-    for (var i = 0; i < words.length; i++) {
-        if (row.text().toLowerCase().indexOf(words[i]) === -1) {
-            row.addClass('hidden');
-            return;
-        }
+class ShuffleTable {
+    
+    constructor(selector) {
+        this.table = $(selector);
+        this.run();
     }
-    row.removeClass('hidden');
+    
+    _sort() {
+    	let element = $("#shuffle-sort option:selected")[0],
+    		reverse = true ? element.getAttribute("data-reverse") : false,
+            sortBy  = element.getAttribute("data-sortby"),
+            rows    = this.table.find('tbody tr:not(.no-data)');
+        
+        for (var i = 0; i < rows.length; i++) {
+            let sortnr = parseFloat(rows[i].text());
+        }
+    };
+    
+    _search() {
+		let text  = $('#shuffle-search').val().toLowerCase(),
+            words = text.split(' ');
+        
+        console.log(this.table)
+        this.table.find('tbody tr:not(.no-data)').each(function() {
+            console.log($(this))
+            for (var i = 0; i < words.length; i++) {
+                if ($(this).text().toLowerCase().indexOf(words[i]) === -1) {
+                    $(this).addClass('hidden');
+                    return;
+                }
+            }
+            $(this).removeClass('hidden');
+        });
+    }
+    
+    _filterCompleted() {
+        $('.shuffle-checkbox').each((i, elem) => {
+        	let checked = elem.checked,
+        		group   = checked ? elem.getAttribute('data-group') : 'all';
+            this.table.find('tbody tr:not(.no-data)').each(function() {
+                if (!$(this).data('groups').includes(group)) {
+                    $(this).addClass('hidden');
+                    return;
+                }
+                $(this).removeClass('hidden');
+            });
+        });
+    }
+    
+    run() {
+		this._search();
+        // this._sort();
+        this._filterCompleted();
+    }
 }
+
+
+$('.shuffle-table').each(function() {
+    let id           = $(this).attr('id'),
+        shuffleTable = new ShuffleTable(id);
+    
+	$('.shuffle-checkbox').on('change', function() {
+		shuffleTable.run();
+	});
+
+	$('#shuffle-sort').on('change', function() {
+        shuffleTable.run();
+	});
+
+	$('#shuffle-search').on('keyup', function() {
+		shuffleTable.run();
+	});
+});

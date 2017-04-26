28e1e03998e4612c959bf9fa384df831d3503f42
Mendes <alex.mendes@bl.uk>
2017-04-26 15:48:59 +0100

    Improve shuffle tables

--- static/js/shuffle.js
+++ static/js/shuffle.js
@@ -45,6 +45,9 @@ class ShuffleGrid {
         });
     }
     
+    /**
+     * Show placeholders if there are no visible grid items.
+     */
     _showPlaceholders() {
         if (this.grid.height() === 0) {
             $('.shuffle-grid-placeholder').show();
@@ -54,28 +57,105 @@ class ShuffleGrid {
     }
     
     run() {
+        this._filterCompleted();
 		this._search();
         this._sort();
-        this._filterCompleted();
         this._showPlaceholders();
     }
 }
 
 
-if ($('#shuffle-grid').length === 1) {
-    let shuffleGrid = new ShuffleGrid('#shuffle-grid');
+/**
+ * A class for handling shuffleable tables.
+ */
+class ShuffleTable {
     
-	$('.shuffle-checkbox').on('change', function() {
-		shuffleGrid.run();
-	});
+    constructor(selector) {
+        this.table = $(selector);
+        this.run();
+    }
+    
+    /**
+     * Sort rows according to selected sort option.
+     */
+    _sort() {
+    	let element      = $("#shuffle-sort option:selected")[0],
+    		reverse      = element.getAttribute("data-reverse") || false,
+            sortBy       = element.getAttribute("data-sortby"),
+            rows         = this.table.find('tbody tr:not(.no-data)');
+        
+        let sortedTable = this.table.find('tbody tr').sort(function(a, b) {
+            if (reverse) {
+                return b.getAttribute(`data-${sortBy}`) - a.getAttribute(`data-${sortBy}`);
+            } else {
+                return a.getAttribute(`data-${sortBy}`) - b.getAttribute(`data-${sortBy}`);
+            }
+        });
+        
+        this.table.find('tbody').first().html(sortedTable);
+    };
+    
+    /**
+     * Return true if the row contains all search terms, false otherwise.
+     */
+    _search(row) {
+		let text  = $('#shuffle-search').val().toLowerCase(),
+            words = text.split(' ');
+        
+        for (var i = 0; i < words.length; i++) {
+            if (row.text().toLowerCase().indexOf(words[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Return true if the row should be filtered out, false otherwise.
+     */
+    _filter(row) {
+        let filter = false;
+        $('.shuffle-checkbox').each((i, elem) => {
+        	let checked = elem.checked,
+        		group   = checked ? elem.getAttribute('data-group') : 'all';
+            
+            if (!row.data('groups').includes(group)) {
+                filter = true;
+            }
+        });
+        return filter;
+    }
+    
+    /**
+     * Hide rows if filtered or search terms not found, otherwise show and sort.
+     */
+    run() {
+        this.table.find('tbody tr:not(.no-data)').each((i, row) => {
+            if (this._filter($(row)) || !this._search($(row))) {
+                $(row).addClass('hidden');
+            } else {
+                this._sort();
+                $(row).removeClass('hidden');
+            }
+        });
+    }
+}
 
-	$('#shuffle-sort').on('change', function() {
+
+if ($('#project-shuffle').length === 1) {
+    let shuffleGrid  = new ShuffleGrid('#shuffle-grid'),
+        shuffleTable = new ShuffleTable('#shuffle-table');
+    
+	$('.shuffle-control').on('change keyup', function() {
+		shuffleGrid.run();
+        shuffleTable.run();
+	});
+    
+    $('.shuffle-tab').on('shown.bs.tab', function() {
         shuffleGrid.run();
-	});
-
-	$('#shuffle-search').on('keyup', function() {
-		shuffleGrid.run();
-	});
+        shuffleTable.run();
+        shuffleGrid.shuffle.update();
+    });
     
     $(window).on('load', function() {
         shuffleGrid.shuffle.update();

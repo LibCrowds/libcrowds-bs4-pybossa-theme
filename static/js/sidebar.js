94a26f59bbde7d0babccd59c6fb4850dda0bd107
Mendes <alex.mendes@bl.uk>
2017-05-04 19:15:28 +0100

    Update sidebar

---
+++ static/js/sidebar.js
@@ -0,0 +1,6 @@
+$('[data-toggle="sidebar-collapse"]').on('click', function(evt) {
+    let target = $(this).data('target');
+    $(target).toggleClass('active');
+    evt.preventDefault();
+    console.log(toggle);
+});

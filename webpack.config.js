c9115d817874d5e61847735bb928adf63b91b6be
Alexander Mendes <alexanderhmendes@gmail.com>
2017-05-13 14:42:09 +0100

    @UpdateUpdate build process

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,13 @@ const webpack = require('webpack'),
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const srcRoot  = './templates/_layouts/src/',
-      distRoot = './templates/_layouts/dist/',
-
-
 let config = {
     entry: {
-        main: path.resolve("./static/js/main.js"),
-        editor: path.resolve("./static/js/editor.js")
+        main: "./static/js/main.js",
+        editor: "./static/js/editor.js"
     },
     output: {
-        path: path.resolve("./static/dist/js"),
+        path: path.resolve(__dirname, 'static/dist/js'),
         filename: "[name].bundle.js",
       },
     module: {
@@ -24,7 +20,8 @@ let config = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }        ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -36,9 +33,8 @@ let config = {
         new HtmlWebpackPlugin({
             hash: true,
             inject: false,
-            chunks: ['main'],
-            filename: path.join(distRoot, 'base.html'),
-            template: path.join(srcRoot, 'base.html')
+            filename: path.resolve(__dirname, 'templates/base.html'),
+            template: path.resolve(__dirname, 'templates/_base.webpack.html')
         })
     ]
 };

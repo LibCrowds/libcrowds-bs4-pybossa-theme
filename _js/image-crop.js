cd86222fab1df315b1be567bdbb4544fabc6ddff
Mendes <alex.mendes@bl.uk>
2017-05-17 17:12:00 +0100

    Fix image cropping

--- _js/image-crop.js
+++ _js/image-crop.js
@@ -1,39 +1,37 @@
-import croppie from 'croppie';
+import Croppie from 'croppie';
 
 let imageCrop;
 
-if ($('#uploadPreview').length) {
-    var $uploadCrop;
-    var $preview = $('#uploadPreview');
-
-    $uploadCrop = $preview.croppie({
+if (document.getElementById('uploadPreview')) {
+    let preview = document.getElementById('uploadPreview');
+    
+    let croppie = new Croppie(preview, {
         viewport: {
-            width: $preview.width() || 300,
-            height: $preview.height() || 300,
-            type: $preview.hasClass('crop-circle') ? 'circle' : 'square'
+            width: preview.width || 300,
+            height: preview.height || 300,
+            type: preview.classList.contains('crop-circle') ? 'circle' : 'square'
         },
         boundary: {
-            width: $preview.width() || 300,
-            height: $preview.height() || 300
+            width: preview.width || 300,
+            height: preview.height || 300
         },
         update: function (data) {
-            $("#x1").val(Math.floor(data.points[0]));
-            $("#y1").val(Math.floor(data.points[1]));
-            $("#x2").val(Math.floor(data.points[2]));
-            $("#y2").val(Math.floor(data.points[3]));
+            document.getElementById("x1").value = Math.floor(data.points[0]);
+            document.getElementById("y1").value = Math.floor(data.points[1]);
+            document.getElementById("x2").value = Math.floor(data.points[2]);
+            document.getElementById("y2").value = Math.floor(data.points[3]);
         }
     });
 
-    $('#avatar').on('change', function () {
-        var input = this;
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (evt) {
-                $uploadCrop.croppie('bind', {
+    document.getElementById('avatar').addEventListener('change', function () {
+        if (this.files && this.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function(evt) {
+                croppie.bind({
                     url: evt.target.result
                 });
             };
-            reader.readAsDataURL(input.files[0]);
+            reader.readAsDataURL(this.files[0]);
         }
     });
 }

e33bb7f127a026561c3bbca509aaa16d609bc212
Alexander Mendes <alexanderhmendes@gmail.com>
2017-05-23 03:21:27 +0100

    Add svg-url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -55,8 +55,11 @@ let config = {
             {   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff"
             },
-            {   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            {   test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000!file-loader"
+            },
+            {   test: /\.svg$/,
+                loader: "svg-url-loader"
             },
             {
                 test: require.resolve("pace-progress"),
